406caa21d586d13785dfb6e087b727ca561c75d4
Arnav495 <arnieincyberland@gmail.com>
2024-10-31 21:10:42 -0700

    Rewrote setup_jetson.sh

--- scripts/setup_jetson.sh
+++ scripts/setup_jetson.sh
@@ -1,7 +1,119 @@
 #!/usr/bin/env bash
 
-echo "THIS SCRIPT IS WIP AND HAS NOT BEEN TESTED!"
+set -euo pipefail
 
-echo "Install Pytorch for the Jetson Nano" 
-chmod u+x install_pytorch_jetson.sh
-./install_pytorch_jetson.sh
+echo "THIS SCRIPT IS WIP AND HAS NOT BEEN TESTED!"
+read -rp "Are you sure you want to continue? [yN]
+>" toContinue
+
+if [[ ! ($toContinue == "yes" || $toContinue == "y") ]]; then
+	echo "Exiting"
+	exit 0
+fi
+
+if ! git rev-parse &>/dev/null --is-inside-work-tree; then
+	echo "Please run this script inside the repo"
+	exit 1
+fi
+
+# screw good practice (it's probably just ironclaw anyway)
+read -rp "Please enter the device password
+>" PASSWORD
+
+echo -n "Checking if password is correct... "
+
+if sudo -kS <<<"$PASSWORD" &>>/dev/null true; then
+	echo "Done"
+else
+	echo "Failed"
+	exit 1
+fi
+
+echo -n "Checking for internet connection (trying to connect to github.com)... "
+
+if ping -i 0.2 -w 1 github.com &>>/dev/null; then
+	echo "Done"
+else
+	echo "Failed"
+	exit 1
+fi
+
+REPOSITORY="$(git rev-parse --show-toplevel)"
+DATA_DIR="$HOME/Downloads/autosetup"
+LOG_FILE="$REPOSITORY/setup.log"
+
+mkdir -p "$DATA_DIR"
+
+if [[ $REPOSITORY == "" ]]; then
+	echo "Repository is null, exiting"
+	exit 1
+fi
+
+if [[ $DATA_DIR == "" ]]; then
+	echo "Temp dir is null, exiting"
+	exit 1
+fi
+
+echo "Logging to $LOG_FILE, please use that for detailed info"
+
+function runCommand() {
+	echo -n "Running \"$*\"... "
+	echo "**Running \"$*\" in \"$(pwd)\"**" > "$LOG_FILE"
+	if "$@" &>>"$LOG_FILE"; then
+		echo "Done"
+	else
+		echo "Failed -- check log for more info"
+		exit 1
+	fi
+}
+
+function runAsRoot() {
+	echo -n "Running \"$*\"... "
+	echo "**Running \"$*\" in \"$(pwd)\" as root**" > "$LOG_FILE"
+	# shellcheck disable=SC2024
+	if sudo <<<"$PASSWORD" -S "$@" &>>"$LOG_FILE"; then
+		echo "Done"
+	else
+		echo "Failed -- check log for more info"
+		exit 1
+	fi
+}
+
+cd "$REPOSITORY"
+
+echo "RUNNING SYSTEM UPGRADE"
+runAsRoot apt-get update -y
+runAsRoot apt-get upgrade -y
+
+echo "INSTALLING JETPACK"
+runAsRoot apt-get install -y nvidia-jetpack
+
+echo "RUNNING PIP COMMANDS"
+runCommand python3 -m venv venv
+# shellcheck disable=SC1091
+source ./venv/bin/activate
+runCommand pip install -r requirements.txt
+
+echo "CLONING PYTORCH"
+cd "$DATA_DIR"
+runCommand git clone --recursive "https://github.com/pytorch/pytorch.git"
+
+echo "BUILDING PYTORCH"
+# TODO: export required env vars here
+cd pytorch
+runAsRoot apt-get install build-essential cmake ninja
+runCommand pip install -r requirements.txt
+runCommand python3 setup.py --install
+
+echo "CLONING TORCHVISION"
+cd "$DATA_DIR"
+runCommand git clone --recursive "https://github.com/pytorch/vision.git"
+
+echo "BUILDING TORCHVISION"
+cd vision
+runCommand pip install -r requirements.txt
+runCommand python3 setup.py --install
+
+echo "SETUP COMPLETE"
+echo "Please restart now."
+
