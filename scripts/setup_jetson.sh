d65c0590805b3d254ad64f5072a6858e412e37ae
Arnav495 <arnieincyberland@gmail.com>
2025-02-15 16:04:16 -0800

    Enviroment variables == happiness

--- scripts/setup_jetson.sh
+++ scripts/setup_jetson.sh
@@ -128,10 +128,16 @@ function cloneIf() {
 }
 
 export USE_PRIORITIZED_TEXT_FOR_LD=1
-export USE_NATIVE_ARCH=1 # stop pytorch doing dumb stuff
+
+# stop pytorch doing dumb stuff
+export USE_NATIVE_ARCH=1
+export NO_DISTRIBUTED=1
+export USE_DISTRIBUTED=0
+export USE_NCCL=0
+
 export CUDACXX="$(realpath "/usr/local/cuda/bin/nvcc")"
 #assertCommand "if cuda compiler exists" test -f "$CMAKE_CUDA_COMPILER"
-export CUDA_HOME="/usr/local/cuda"
+export CUDA_HOME="/usr/local/cuda/" # this *might* need a /bin/ at the end
 #assertCommand "if cuda home exists" test -d "$CUDA_HOME"
 export MAX_JOBS=1 # for memory reasons
 #export FORCE_CUDA=1
