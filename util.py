90362c5b3e01c0043d5cf172f115bb8f31d8b611
Arnav495 <arnieincyberland@gmail.com>
2025-03-24 16:38:08 -0700

    redo changes

--- util.py
+++ util.py
@@ -2,29 +2,31 @@ import math
 from ultralytics.engine.results import Boxes # type: ignore
 
 # TODO: Change these to the actual camera values
-fov = [70, 43.75] # Arducam
-RESOLUTION = [720, 1280]
+FOV = [70, 43.75] # Arducam
 # fov = [59.703, 33.583] # Microsoft lifecam or other cameras with diagonal FOV of 68.5 degrees and 1280x720 resolution
 
 def get_fovs(box: Boxes) -> list[float]:
+    if len(box[0]) == 0:
+        return
+
     x1, y1, x2, y2 = box.xyxy[0]
     center: list[float] = [(x1+x2)/2,(y1+y2)/2]
-    zero_centered: list[float] = [center[0] - (RESOLUTION[0]/2), center[1] - (RESOLUTION[1]/2)]
-    fovs: list[float] = [math.tan(zero_centered[0]) * (fov[0]/2), math.tan(zero_centered[1]) * (fov[1]/2)]
+    zero_centered: list[float] = [center[0] - (box.orig_shape[0]/2), center[1] - (box.orig_shape[1]/2)]
+    fovs: list[float] = [math.tan(zero_centered[0]) * (FOV[0]/2), math.tan(zero_centered[1]) * (FOV[1]/2)]
     return fovs
 
 def get_x_offset_deg(box: Boxes) -> float:
     #source: Limelight docs(LINK HERE)
 
     if len(box[0]):
-        hfov = fov[0]*(math.pi/180)
+        hfov = FOV[0]*(math.pi/180)
         x1, y1, x2, y2 = box.xyxy[0]
 
         bbox_center_coord = [(x1+x2)/2,(y1+y2)/2]
 
         cx = bbox_center_coord[0]
 
-        nx = (cx-320)/320
+        nx = (cx-(box.orig_shape[1]/2))/(box.orig_shape[0]/2)
 
         vw = 2*math.tan((hfov/2))
 
@@ -40,14 +42,14 @@ def get_y_offset_deg(box: Boxes) -> float:
     #source: Limelight docs(LINK HERE)
 
     if len(box[0]):
-        vfov = fov[1]*(math.pi/180)
+        vfov = FOV[1]*(math.pi/180)
         x1, y1, x2, y2 = box.xyxy[0]
 
         bbox_center_coord = [(x1+x2)/2,(y1+y2)/2]
 
         cy = bbox_center_coord[1]
 
-        ny = (cy-320)/320
+        ny = (cy-(box.orig_shape[0]/2))/(box.orig_shape[1]/2)
 
         vh = 2*math.tan((vfov/2))
 
