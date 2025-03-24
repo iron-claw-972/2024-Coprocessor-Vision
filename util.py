0aad023c6bf089eaec14371e6b61d704402689a7
Arnav495 <arnieincyberland@gmail.com>
2025-03-24 15:58:35 -0700

    Add alternative code for fov calc.

--- util.py
+++ util.py
@@ -2,8 +2,17 @@ import math
 from ultralytics.engine.results import Boxes # type: ignore
 
 # TODO: Change these to the actual camera values
-# fov = [70, 43.75] # Arducam
-fov = [59.703, 33.583] # Microsoft lifecam or other cameras with diagonal FOV of 68.5 degrees and 1280x720 resolution
+fov = [70, 43.75] # Arducam
+RESOLUTION = [720, 1280]
+# fov = [59.703, 33.583] # Microsoft lifecam or other cameras with diagonal FOV of 68.5 degrees and 1280x720 resolution
+
+def get_fovs(box: Boxes) -> list[float]:
+    x1, y1, x2, y2 = box.xyxy[0]
+    center: list[int] = [(x1+x2)/2,(y1+y2)/2]
+    zero_centered: list[int] = [center[0] - (RESOLUTION[0]/2), center[1] - (RESOLUTION[1]/2)]
+    viewport: list[int] = [zero_centered[0], zero_centered[1], 1]
+    fovs: list[int] = [math.tan(viewport[0]) * (fov[0]/2), math.tan(viewport[1]) * (fov[1]/2)]
+    return fovs
 
 def get_x_offset_deg(box: Boxes) -> float:
     #source: Limelight docs(LINK HERE)
