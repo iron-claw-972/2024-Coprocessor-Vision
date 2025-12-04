8b5ddb3783c2655b06ae96926c35fa53492e4ea0
iefomit <timofei.stem@gmail.com>
2025-12-03 17:55:45 -0800

    auto formatter, no code changes

--- detect.py
+++ detect.py
@@ -3,8 +3,8 @@ from threading import Thread
 import time
 import cv2
 import numpy as np
-from ultralytics import YOLO # type: ignore
-from ultralytics.engine.results import Results # type: ignore
+from ultralytics import YOLO  # type: ignore
+from ultralytics.engine.results import Results  # type: ignore
 import time
 import ntables
 import snapshotter
@@ -27,15 +27,21 @@ is_interactive: bool = sys.stderr.isatty()
 # disable when not needed to improve performance
 enable_mjpeg: bool = is_interactive
 
-@functools.cache # only run once
+
+@functools.cache  # only run once
 def get_ips() -> list[str]:
     ip_list: list[str] = []
-    if (platform.system() == "Linux"): # screw windows
-       ipstr: str = subprocess.check_output(["hostname", "-I"]) \
-           .decode("UTF-8").rstrip("\n").strip()
-       ip_list = ipstr.split(" ")
+    if platform.system() == "Linux":  # screw windows
+        ipstr: str = (
+            subprocess.check_output(["hostname", "-I"])
+            .decode("UTF-8")
+            .rstrip("\n")
+            .strip()
+        )
+        ip_list = ipstr.split(" ")
     ip_list.append("localhost")
     return ip_list
+
 
 # Define the video files for the trackers
 # Path to video files, 0 for webcam, 1 for external camera
@@ -43,22 +49,25 @@ def get_ips() -> list[str]:
 # TODO: This should only be the cameras we need, not every possibility
 cameras: list[int] = [i for i in range(4)]
 
+
 def handle_signal(signalnum, stack_frame):
     raise SystemExit()
+
 
 # handle SIGTERM nicely
 signal.signal(signal.SIGTERM, handle_signal)
 
 # Load the model
-model = YOLO('models/best_Reefscape_2025_model.engine')
+model = YOLO("models/best_Reefscape_2025_model.engine")
 
 # exit gracefully on ^C
 is_interrupted: bool = False
 print("Hello World")
 
+
 def run_cam_in_thread(cameraname: int, file_index: int, q: Queue) -> None:
     video: cv2.VideoCapture = cv2.VideoCapture(cameraname)  # Read the video file
-    #video.set(cv2.CAP_PROP_BUFFERSIZE, 0) # doesn't work :(
+    # video.set(cv2.CAP_PROP_BUFFERSIZE, 0) # doesn't work :(
 
     while True:
         ret: bool
@@ -69,7 +78,7 @@ def run_cam_in_thread(cameraname: int, file_index: int, q: Queue) -> None:
         # exit if no frames remain
         if not ret:
             break
-    
+
         if is_interrupted:
             break
 
@@ -90,7 +99,6 @@ def run_cam_in_thread(cameraname: int, file_index: int, q: Queue) -> None:
     print(f"CAMERA {cameraname} EXITING (camera thread)")
     # Release video sources
     video.release()
-        
 
 
 def run_tracker_in_thread(cameraname: int, file_index: int, stream: Stream) -> None:
@@ -109,7 +117,9 @@ def run_tracker_in_thread(cameraname: int, file_index: int, stream: Stream) -> N
     """
 
     q: Queue = Queue(maxsize=1)
-    cam_thread = Thread(target=run_cam_in_thread, args=(cameraname, file_index, q), daemon=False)
+    cam_thread = Thread(
+        target=run_cam_in_thread, args=(cameraname, file_index, q), daemon=False
+    )
     cam_thread.start()
 
     snapshot_time: float = time.time()
@@ -118,45 +128,71 @@ def run_tracker_in_thread(cameraname: int, file_index: int, stream: Stream) -> N
 
     # Exit the loop if no more frames in the video
     while not is_interrupted and cam_thread.is_alive():
-        if (is_interactive):
-            print(f"Camera: {cameraname}") # For debugging 
-
+        if is_interactive:
+            print(f"Camera: {cameraname}")  # For debugging
 
         try:
             start_time: float
             frame: np.ndarray
             frame, start_time = q.get(block=True, timeout=5)
-        except Empty: # stop the thread getting stuck if the camera thread immidiately dies
+        except (
+            Empty
+        ):  # stop the thread getting stuck if the camera thread immidiately dies
             continue
 
-        #start_time: float = time.time()
+        # start_time: float = time.time()
 
         # Track objects in frames if available
         results: list[Results] = model.predict(frame, verbose=is_interactive)
-        #time.sleep(0.2)
+        # time.sleep(0.2)
         res_plotted: np.ndarray = results[0].plot()
         # Calculate offsets and add to NetworkTables
         ntables.add_results(results, start_time, file_index)
 
         end_time: float = time.time()
 
-        if results[0] is not None and len(results[0].boxes) != 0 and len(results[0].boxes[0]) is not None:
+        if (
+            results[0] is not None
+            and len(results[0].boxes) != 0
+            and len(results[0].boxes[0]) is not None
+        ):
             xyxy = results[0].boxes.xyxy[0]
             orig_shape = results[0].boxes.orig_shape
             print("x: " + str(util.get_x_offset_deg_single(xyxy, orig_shape)))
             print("y: " + str(util.get_y_offset_deg_single(xyxy, orig_shape)))
 
-        #if (time.time() - snapshot_time > 10): # snapshot every x seconds
-            #snapshotter.submit(results[0])
-            #snapshot_time = time.time()
+        # if (time.time() - snapshot_time > 10): # snapshot every x seconds
+        # snapshotter.submit(results[0])
+        # snapshot_time = time.time()
 
-        if (enable_mjpeg):
-            fps: float = round(1/(end_time-start_time), 2)
-            center: tuple(int, int) = (640//2, 480//2)
+        if enable_mjpeg:
+            fps: float = round(1 / (end_time - start_time), 2)
+            center: tuple(int, int) = (640 // 2, 480 // 2)
             size: int = 25
-            cv2.line(res_plotted, (center[0] - size, center[1]), (center[0] + size, center[1]), (0, 128, 255), 5)
-            cv2.line(res_plotted, (center[0], center[1] - size), (center[0], center[1] + size), (0, 128, 255), 5)
-            cv2.putText(res_plotted, str(fps), (7, 70), cv2.FONT_HERSHEY_SIMPLEX , 3, (100, 255, 0), 3, cv2.LINE_AA) 
+            cv2.line(
+                res_plotted,
+                (center[0] - size, center[1]),
+                (center[0] + size, center[1]),
+                (0, 128, 255),
+                5,
+            )
+            cv2.line(
+                res_plotted,
+                (center[0], center[1] - size),
+                (center[0], center[1] + size),
+                (0, 128, 255),
+                5,
+            )
+            cv2.putText(
+                res_plotted,
+                str(fps),
+                (7, 70),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                3,
+                (100, 255, 0),
+                3,
+                cv2.LINE_AA,
+            )
             print("FPS: " + str(fps))
 
             stream.set_frame(res_plotted)
@@ -165,7 +201,7 @@ def run_tracker_in_thread(cameraname: int, file_index: int, stream: Stream) -> N
     print(f"CAMERA {cameraname} EXITING (detector thread)")
 
 
-if (enable_mjpeg):
+if enable_mjpeg:
     stream: Stream = Stream("Detectorator", size=(640, 480), quality=50, fps=10)
     server: MjpegServer = MjpegServer("0.0.0.0", 5090)
     server.add_stream(stream)
@@ -177,7 +213,9 @@ threads: list[Thread] = []
 for i in range(len(cameras)):
     # Create the thread
     # daemon=True makes it shut down if something goes wrong
-    thread = Thread(target=run_tracker_in_thread, args=(cameras[i], i, stream), daemon=True)
+    thread = Thread(
+        target=run_tracker_in_thread, args=(cameras[i], i, stream), daemon=True
+    )
     # Add to the array to use later
     threads.append(thread)
     # Start the thread
@@ -197,7 +235,6 @@ except (KeyboardInterrupt, SystemExit) as e:
 for thread in threads:
     thread.join()
 
-if (enable_mjpeg):
+if enable_mjpeg:
     # Clean up
     server.stop()
-
--- ntables.py
+++ ntables.py
@@ -2,7 +2,7 @@ import ntcore
 import util
 import time
 from enum import Enum
-from ultralytics.engine.results import Results # type: ignore
+from ultralytics.engine.results import Results  # type: ignore
 
 nt_inst = ntcore.NetworkTableInstance.getDefault()
 
@@ -10,18 +10,26 @@ pub_sub_options = ntcore.PubSubOptions(sendAll=True)
 
 table = nt_inst.getTable("object_detection")
 distance_topic = table.getDoubleArrayTopic("distance").publish(options=pub_sub_options)
-x_angle_offset_topic = table.getDoubleArrayTopic("x_offset").publish(options=pub_sub_options)
-y_angle_offset_topic = table.getDoubleArrayTopic("y_offset").publish(options=pub_sub_options)
+x_angle_offset_topic = table.getDoubleArrayTopic("x_offset").publish(
+    options=pub_sub_options
+)
+y_angle_offset_topic = table.getDoubleArrayTopic("y_offset").publish(
+    options=pub_sub_options
+)
 object_class_topic = table.getStringArrayTopic("class").publish(options=pub_sub_options)
-camera_index_topic = table.getIntegerArrayTopic("index").publish(options=pub_sub_options)
+camera_index_topic = table.getIntegerArrayTopic("index").publish(
+    options=pub_sub_options
+)
 latency_topic = table.getDoubleArrayTopic("latency").publish(options=pub_sub_options)
 flippy_topic = table.getBooleanTopic("flippy").publish(options=pub_sub_options)
+
 
 class ObjClasses(Enum):
     ALGAE = 0
     CORAL = 1
 
-#distance: list[float] = []
+
+# distance: list[float] = []
 x_offset: list[float] = []
 y_offset: list[float] = []
 object_class: list[str] = []
@@ -32,29 +40,37 @@ last_flippy: bool = False
 nt_inst.startClient4("Coprocessor")
 nt_inst.setServerTeam(972)
 
+
 def publish_distance() -> None:
     raise NotImplementedError("distance is not not implemented yet")
-    #distance_topic.set(distance)
+    # distance_topic.set(distance)
+
 
 def publish_x_angle_offset() -> None:
     x_angle_offset_topic.set(x_offset)
 
+
 def publish_y_angle_offset() -> None:
     y_angle_offset_topic.set(y_offset)
+
 
 def publish_class() -> None:
     object_class_topic.set(object_class)
 
+
 def publish_camera_index() -> None:
     camera_index_topic.set(camera_index)
 
+
 def publish_latency() -> None:
     latency_topic.set(latency_list)
+
 
 def flip_flippy() -> None:
     global last_flippy
     last_flippy = not last_flippy
     flippy_topic.set(last_flippy)
+
 
 def add_results(results: list[Results], start_time: float, index: int) -> None:
     global nt_inst
@@ -64,7 +80,7 @@ def add_results(results: list[Results], start_time: float, index: int) -> None:
         if camera_index[i] == index:
             x_offset.pop(i)
             y_offset.pop(i)
-            #distance.pop(i)
+            # distance.pop(i)
             object_class.pop(i)
             camera_index.pop(i)
             latency_list.pop(i)
@@ -80,12 +96,12 @@ def add_results(results: list[Results], start_time: float, index: int) -> None:
             cls_id = int(boxes.cls[box_idx].cpu().numpy())
             x_offset.append(util.get_x_offset_deg_single(xyxy, boxes.orig_shape))
             y_offset.append(util.get_y_offset_deg_single(xyxy, boxes.orig_shape))
-            #distance.append(util.get_distance(box))
+            # distance.append(util.get_distance(box))
             object_class.append(result.names[cls_id])
             camera_index.append(index)
             latency_list.append(time.time() - start_time)
     # Publish values to NetworkTables
-    #publish_distance()
+    # publish_distance()
     publish_x_angle_offset()
     publish_y_angle_offset()
     publish_class()
@@ -93,4 +109,3 @@ def add_results(results: list[Results], start_time: float, index: int) -> None:
     publish_latency()
     flip_flippy()
     nt_inst.flush()
-    
